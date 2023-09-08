39e7aa44061798a1fee4b6bb24fa423ddcd370cf
lucas <lucas.fnt18@gmail.com>
2023-09-08 19:16:32 -0300

    Alterado atribuição de atributos com setters; Importado pacote modelos;

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,11 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1972;
-        meuFilme.duracaoEmMinutos = 175;
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1972);
+        meuFilme.setDuracaoEmMinutos(175);
 
         meuFilme.exibeFichaTecnica();
 
@@ -11,8 +13,7 @@ public class Principal {
         meuFilme.avalia(5);
         meuFilme.avalia(10);
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
 
         System.out.println(meuFilme.pegaMedia());
     }
