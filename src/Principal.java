7084e11a7e4faf787a8867ab6b4512cb24b7f24d
lucas <lucas.fnt18@gmail.com>
2023-09-13 18:44:01 -0300

    Importado classe CalculadoraDeTempo;
    Feito nova instancia de Filme (avatar) incluindo dados da ficha tecnica;
    
    Instanciado objeto (calculadora) da classe CalculadoraDeTempo para testar funcionalidades dos calculos;

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,4 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -7,6 +8,7 @@ public class Principal {
         meuFilme.setNome("O Poderoso Chefão");
         meuFilme.setAnoDeLancamento(1972);
         meuFilme.setDuracaoEmMinutos(175);
+        System.out.println("A duração em minutos é: " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
 
@@ -23,6 +25,19 @@ public class Principal {
         lost.setAnoDeLancamento(2004);
         lost.setTemporadas(6);
         lost.setEpisodiosPorTemporada(25);
+        lost.setMinutosPorEpisodio(50);
         lost.exibeFichaTecnica();
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2009);
+        meuFilme.setDuracaoEmMinutos(162);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println("Tempo total: " + calculadora.getTempoTotal());
     }
 }
